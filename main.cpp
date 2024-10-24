7ece3c9211e5e109300ed7bb27ce50b226c7bbe9
winstonjose01 <winston.jose@gmail.com>
2024-10-24 10:21:34 -0700

    revision-4

--- main.cpp
+++ main.cpp
@@ -8,7 +8,7 @@ using namespace std;
 const int SZ_NAMES = 200, SZ_COLORS = 25, MAX_AGE = 20, MIN = 5, MAX = 20;
 
 int select_goat(list<Goat> trip);
-void delete_goat(list<Goat> &trip);
+void delete_goat(list<Goat> &trip, int i);
 void add_goat(list<Goat> &trip, string [], string []);
 void display_trip(list<Goat> trip);
 int main_menu();
@@ -32,19 +32,26 @@ int main() {
 
     list <Goat> goat_gang;
     for (int i = 0; i < (MIN + rand() % MAX-MIN + 1); i++){
-        goat_gang.push_back(Goat(names[i], rand() % (MAX_AGE+1), colors[rand() % SZ_COLORS]));
+        goat_gang.push_back(Goat(names[rand() % SZ_NAMES], rand() % (MAX_AGE+1), colors[rand() % SZ_COLORS]));
     }
 
     while (true){
         choice = main_menu();
         switch (choice){
-            case 1: add_goat(goat_gang, &names[rand() % SZ_NAMES] , &colors[rand() % SZ_COLORS]);
+            case 1: add_goat(goat_gang, names, colors);
+                    display_trip (goat_gang);
                 break;
-            case 2: delete_goat;
+            case 2: int selection = select_goat(goat_gang);
+                    delete_goat(goat_gang, selection);
+                    display_trip (goat_gang);
                 break;
+            case 3: display_trip(goat_gang);
+                break;
+            case 4:
+                return 0;
             
         }
-        }
+    }
 
     return 0;
 }
@@ -56,17 +63,35 @@ int main_menu(){
     cout << "[2] Delete a goat" << endl;
     cout << "[3] List goats" << endl;
     cout << "[4] Quit" << endl;
+    cout << "Choice --> ";
+
     cin >> choice;
     return choice;
     }
 
-void add_goat(list<Goat> &trip, string n, string c){
-    trip.push_back(Goat(n, rand() % (MAX_AGE+1), c));
+void add_goat(list<Goat> &trip, string n[], string c[]){
+    trip.push_back(Goat(n[rand() % SZ_NAMES], rand() % (MAX_AGE+1), c[rand() % SZ_COLORS]));
+}
+
+void display_trip (list<Goat> trip){
+    if i 
     for (auto goat : trip){
-        cout << "\t" << goat.get_name() << " (" << goat.get_age() << ", " << goat.get_color() << endl;
+        int i = 1;
+        cout << "\t[" << i++ << "] " << goat.get_name() << " (" << goat.get_age() << ", " << goat.get_color() << ")" << endl;
     }
 }
 
-void delete_goat (list<Goat>){
+void delete_goat (list<Goat> &trip, int selection){
+    int i = 1;
+    for (auto goat : trip){
+        cout << "\t[" << i << "] " << goat.get_name() << " (" << goat.get_age() << ", " << goat.get_color() << ")" << endl;
+    }
 
+}
+
+int select_goat (list <Goat> trip){ 
+    int selection;
+    display_trip(trip);
+    cout << "Which goat is staying behind ? --> ";
+    cin >> selection;
 }
