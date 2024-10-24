53a7e0307e664d1ec9b1335310f0ad2bd5b0f3d9
winstonjose01 <winston.jose@gmail.com>
2024-10-24 09:16:56 -0700

    revision-3

--- main.cpp
+++ main.cpp
@@ -5,7 +5,7 @@
 #include "Goat.h"
 using namespace std;
 
-const int SZ_NAMES = 200, SZ_COLORS = 25, MAX_AGE = 20;
+const int SZ_NAMES = 200, SZ_COLORS = 25, MAX_AGE = 20, MIN = 5, MAX = 20;
 
 int select_goat(list<Goat> trip);
 void delete_goat(list<Goat> &trip);
@@ -16,6 +16,7 @@ int main_menu();
 int main() {
     srand(time(0));
     bool again;
+    int choice;
 
     // read & populate arrays for names and colors
     ifstream fin("names.txt");
@@ -30,15 +31,17 @@ int main() {
     fin1.close();
 
     list <Goat> goat_gang;
-    for (int i = 0; i < 8; i++){
+    for (int i = 0; i < (MIN + rand() % MAX-MIN + 1); i++){
         goat_gang.push_back(Goat(names[i], rand() % (MAX_AGE+1), colors[rand() % SZ_COLORS]));
     }
 
-    int choice = main_menu();
-    while (choice !=4){
+    while (true){
+        choice = main_menu();
         switch (choice){
-            case 1: add_goat(goat_gang,)
-                break
+            case 1: add_goat(goat_gang, &names[rand() % SZ_NAMES] , &colors[rand() % SZ_COLORS]);
+                break;
+            case 2: delete_goat;
+                break;
             
         }
         }
@@ -57,6 +60,13 @@ int main_menu(){
     return choice;
     }
 
-void add_goat(list<Goat> &trip, string [], string []){
-    
+void add_goat(list<Goat> &trip, string n, string c){
+    trip.push_back(Goat(n, rand() % (MAX_AGE+1), c));
+    for (auto goat : trip){
+        cout << "\t" << goat.get_name() << " (" << goat.get_age() << ", " << goat.get_color() << endl;
+    }
+}
+
+void delete_goat (list<Goat>){
+
 }
